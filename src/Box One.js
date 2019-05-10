0b45974eeff394bed6cb2f0adda3233692dbc229
unknown <811706959@qq.com>
2019-05-10 13:27:44 +0800

    first commit

---
+++ src/Box One.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './BoxOne.css';
+
+class X extends Component{
+  render(){
+    return(
+        <div class="BoxOne">
+            <div>BoxOne</div>
+        </div>
+    )
+  }
+}
+
+export default X;
